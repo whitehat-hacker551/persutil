31a68c05cdac41e11cdbf105f30fb2c01c993aa7
Rafael Aznar <rafaaznar@gmail.com>
2025-11-10 12:05:42 +0100

    Primer commit

---
+++ src/main/java/net/ausiasmarch/persutil/PersutilApplication.java
@@ -0,0 +1,13 @@
+package net.ausiasmarch.persutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersutilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersutilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ausiasmarch/persutil/PersutilApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ausiasmarch.persutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersutilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
